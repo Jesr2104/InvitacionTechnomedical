6d724ab2fd1af67cc683afe05cd27890304d97f5
Jorge Soto Ramos <36306814+Jesr2104@users.noreply.github.com>
2025-07-09 14:02:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("formularioRegistro");
   const mensaje = document.getElementById("mensaje");
+  const boton = document.getElementById("botonSubmit");
 
   form.addEventListener("submit", function (e) {
     e.preventDefault();
 
+    if (boton.classList.contains('bloqueado')) return;
+
+    // Mostrar loader y bloquear visualmente
+      boton.innerHTML = 'Enviando <span class="loader"></span>';
+      boton.classList.add('bloqueado');
+
     const data = new FormData(form);
 
-    fetch("https://script.google.com/macros/s/AKfycbza4uDJSioWGeWeP1mV_28pu8bsuwkqpxMs3BPfzd_7Z9ne-_jZZT7rub5t4jA68ole/exec", {
+    fetch("https://script.google.com/macros/s/AKfycbxlY10bYGGFmOrbaO-lt_YWKr1C4_Woy0urza_XjeIURQFp5jaDygtPc_fVttYV3QLD/exec", {
       method: "POST",
       body: data,
       mode: "no-cors"
@@ -23,3 +30,19 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+function mostrarOtraCiudad(select) {
+    const otraCiudadContainer = document.getElementById('otraCiudadContainer');
+    const otraCiudadInput = document.getElementById('otraCiudad');
+
+    if (select.value === 'otra') {
+      otraCiudadContainer.style.display = 'block';
+      otraCiudadInput.required = true;
+      select.name = 'ciudad_dummy'; // desactiva temporalmente el select como "ciudad"
+    } else {
+      otraCiudadContainer.style.display = 'none';
+      otraCiudadInput.required = false;
+      otraCiudadInput.value = '';
+      select.name = 'ciudad'; // el select vuelve a ser el campo ciudad
+    }
+  }
